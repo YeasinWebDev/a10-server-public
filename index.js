63c22016720b7a6a2b6b4211b455efeb6c3087c4
YeasinWebDev <ar9485727@gmail.com>
2024-04-26 08:22:41 +0600

    mongodb setup

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config();
@@ -10,6 +11,60 @@ app.use(express.json())
 
 app.listen(PORT)
 
+// a-10
+// aeWZHUAqE3IjavWA
+
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+
 app.get('/', (req, res) => {
     res.send('CRUD IS RUNNER')
   })
+
+
+
+  
+const uri = `mongodb+srv://${username}:${password}@cluster0.i0wjdyi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // call of default art 
+    const database =  client.db('default-art')
+    const collection =  database.collection('art-1')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
