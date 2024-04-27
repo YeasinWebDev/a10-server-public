5fda47d5bdabc430728c83e85eebb30c23d863f3
YeasinWebDev <ar9485727@gmail.com>
2024-04-27 21:30:07 +0600

    filter by email

--- index.js
+++ index.js
@@ -18,12 +18,12 @@ const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 
 app.get('/', (req, res) => {
-    res.send('CRUD IS RUNNER')
-  })
+  res.send('CRUD IS RUNNER')
+})
 
 
 
-  
+
 const uri = `mongodb+srv://${username}:${password}@cluster0.i0wjdyi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -38,48 +38,63 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // call of default art 
-    const database =  client.db('default-art')
+    const database = client.db('default-art')
     // collection for art-1
-    const artcollection =  database.collection('art-1')
+    const artcollection = database.collection('art-1')
     // collection for art-2
-    const artcollection2 =  database.collection('art-2')
+    const artcollection2 = database.collection('art-2')
 
-    app.get('/art-1', async(req, res) => {
+    app.get('/art-1', async (req, res) => {
       const result = await artcollection.find().toArray()
       res.send(result)
     })
-    app.post('/art-1', async(req, res) => {
+    app.post('/art-1', async (req, res) => {
       const result = await artcollection.insertOne(req.body)
       res.json(result)
     })
-    app.get('/art-1/:id', async(req, res) => {
+    app.get('/art-1/:id', async (req, res) => {
       const id = req.params.id
-      const quary = {_id: new ObjectId(id)}
+      const quary = { _id: new ObjectId(id) }
       const result = await artcollection.findOne(quary)
       res.send(result)
     })
 
 
     // req for all user collection
-    app.get('/art-2', async(req, res) =>{
-      const result = await artcollection2.find().toArray()
+   
+    app.get('/art-2', async (req, res) => {
+      const email = req.query.email;
+      const allData = req.query.all;
+      if(allData){
+        const result = await artcollection2.find().toArray();
+        res.send(result);
+        return
+      }
+      
+      try {
+        const query = { user_email: email };
+        const result = await artcollection2.find(query).toArray();
+        res.send(result);
+      } catch (error) {
+        console.error( error);
+      }
+    });
+
+
+
+    app.get('/art-2/:id', async (req, res) => {
+      const id = req.params.id
+      const quary = { _id: new ObjectId(id) }
+      const result = await artcollection2.findOne(quary)
       res.send(result)
     })
 
-    app.get('/art-2/:email', async(req, res)=>{
-      const email = req.body
-      console.log(email)
-      const quary = {user_email: email}
-      const result = await artcollection2.find(quary)
-      res.send(result)
-    })
-
-    app.post("/art-2", async (req,res) =>{
+    app.post("/art-2", async (req, res) => {
       const result = await artcollection2.insertOne(req.body)
-      res.json(result)
+      res.send(result)
     })
 
-
+   
 
 
 
